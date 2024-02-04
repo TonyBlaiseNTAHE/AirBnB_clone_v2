e6656c092e9e87400c9e4862141c14440f21781c
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-02-04 22:12:35 +0200

    a Bash script that sets up your web servers for the deployment of web_static.

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -17,7 +17,7 @@ sudo echo "
    Holberton School
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
-ln -s -f /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/listen 80 default server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 sudo service nginx restart
